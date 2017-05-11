211f694ddf724bc13be3d0c7afd5a9886538692a
zhaoziying <zhaoziying@babeltime.com>
2017-05-11 15:08:07 +0800

    修改超时实现。

--- aframwork/src/main/java/org/zzy/aframwork/network/util/NetUtils.java
+++ aframwork/src/main/java/org/zzy/aframwork/network/util/NetUtils.java
@@ -4,8 +4,12 @@ package org.zzy.aframwork.network.util;
 import android.text.TextUtils;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -15,39 +19,39 @@ import okhttp3.Response;
 public class NetUtils {
     public static final MediaType MEDIA_TYPE_JSON
             = MediaType.parse("application/json; charset=utf-8");
-    private static final int CONNECT_TIMEOUT = 300;
 /*******************************************************************************************************/
 
     public static String sendGetRequest(RequestCtx ctx) throws Exception {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        OkHttpClient.Builder builder =  okHttpClient.newBuilder();
-        builder.connectTimeout(ctx.getTimerout(),TimeUnit.SECONDS);
-        builder.readTimeout(ctx.getTimerout(),TimeUnit.SECONDS);
-        builder.writeTimeout(ctx.getTimerout(),TimeUnit.SECONDS);
-//        builder.writeTimeout()
 
-        final Request request = new Request.Builder()
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(ctx.getTimerout(), TimeUnit.SECONDS)
+                .readTimeout(ctx.getTimerout(), TimeUnit.SECONDS)
+                .writeTimeout(ctx.getTimerout(),TimeUnit.SECONDS)
+                .build();
+
+        Request request = new Request.Builder()
                 .url(ctx.getUrl())
                 .build();
-        Response response = okHttpClient.newCall(request).execute();
+        Response response = client.newCall(request).execute();
+
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
         return handleServerData(response.body().string(),response.code(),ctx);
     }
 
     public static String sendPostRequest(RequestCtx ctx) throws Exception {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        OkHttpClient.Builder builder =  okHttpClient.newBuilder();
-        builder.connectTimeout(ctx.getTimerout(),TimeUnit.SECONDS);
-        builder.readTimeout(ctx.getTimerout(),TimeUnit.SECONDS);
-        builder.writeTimeout(ctx.getTimerout(),TimeUnit.SECONDS);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(ctx.getTimerout(), TimeUnit.SECONDS)
+                .readTimeout(ctx.getTimerout(), TimeUnit.SECONDS)
+                .writeTimeout(ctx.getTimerout(),TimeUnit.SECONDS)
+                .build();
 
         Request request = new Request.Builder()
                 .url(ctx.getUrl())
                 .post(RequestBody.create(MEDIA_TYPE_JSON,ctx.getParams().toString()))
                 .build();
 
-        Response response = okHttpClient.newCall(request).execute();
+        Response response = client.newCall(request).execute();
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
         return handleServerData(response.body().string(),response.code(),ctx);
